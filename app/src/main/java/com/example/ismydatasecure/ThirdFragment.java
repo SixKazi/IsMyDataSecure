857234bf53ef38b9a1170c676768dcaa1984af0c
SixKazi` <Bih7YqY3rA5gtAz>
2020-08-21 15:56:46 +0100

    master

--- app/src/main/java/com/example/ismydatasecure/ThirdFragment.java
+++ app/src/main/java/com/example/ismydatasecure/ThirdFragment.java
@@ -1,92 +1,58 @@
 package com.example.ismydatasecure;
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-
-import java.io.UnsupportedEncodingException;
-import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+/*
+*  Class for Fragment Three; Tips
+* */
 
 public class ThirdFragment extends Fragment {
 public SearchView search;
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_third, container, false);
-
     }
-
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        final ArrayList<Tip> tipList = ((MainActivity)getActivity()).genTips();
-
         search = view.findViewById(R.id.search_bar);
 
+        //Generate Tip List with MainActivity Method genTips & Set to RecyclerView
+        final ArrayList<Tip> tipList = ((MainActivity)getActivity()).genTips();
         RecyclerView mRecyclerview = view.findViewById(R.id.tip_view);
         mRecyclerview.setHasFixedSize(true);
         mRecyclerview.setLayoutManager(new LinearLayoutManager(this.getContext()));
-
-
         final RecAdapter recAdapter = new RecAdapter(getContext(), tipList);
         mRecyclerview.setAdapter(recAdapter);
+
+        //Search Bar On Query Listener for instant filtering
         search.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
                 return false;
             }
-
             @Override
             public boolean onQueryTextChange(String newText) {
                 recAdapter.getFilter().filter(newText);
-
                 return false;
             }
         });
+        //OnClickListener to handle click events on Tips, To expand view to show main tip text
         recAdapter.setOnItemClickListener(new RecAdapter.OnItemClickListener() {
             @Override
             public void onItemClick(int position) {
                 ((MainActivity)getActivity()).hideKeyboard();
-
                 boolean expanded = tipList.get(position).isExpanded();
                 tipList.get(position).setExpanded(!expanded);
                 recAdapter.notifyItemChanged(position);
-
             }
         });
-
-
     }
-
 }
-
-/* TODO:
-    Implement GUI improvement for password strength (hsimp jquery? custom java?)
-    help button implementation for explanation of app features
-    tips implementation for help on how to improve security & create stronger passwords*/
